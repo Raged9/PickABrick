4c37928024fc7c0ce5bc8b0ae7dfa139c412c63b
Raged9 <edgar.edmanata@gmail.com>
2025-11-15 20:06:53 +0700

    Commit 3 Christopher:  Login/SignUp dan Modal (ConnectDB)

---
+++ lego_cms/models/User.ts
@@ -0,0 +1,32 @@
+import mongoose, { Schema, model, models, Document } from 'mongoose';
+
+
+export interface IUser extends Document {
+  email: string;
+  username: string;
+  password: string;
+}
+
+const UserSchema: Schema = new Schema({
+  email: {
+    type: String,
+    required: [true, 'Please provide an email.'],
+    unique: true,
+    match: [/.+\@.+\..+/, 'Please provide a valid email address.'],
+  },
+  username: {
+    type: String,
+    required: [true, 'Please provide a username.'],
+    unique: true,
+    minlength: 3,
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password.'],
+    minlength: 6,
+  },
+}
+);
+
+export default (models.User as mongoose.Model<IUser>) || 
+               model<IUser>('User', UserSchema);

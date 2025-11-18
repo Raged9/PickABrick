67889faa2760f37d4ba4d118bcea69e1ba3abbdb
ARYASUTA22 <aryasuta.535240138@stu.untar.ac.id>
2025-11-18 11:38:28 +0700

    Commit 4: Mengubah sidebar menggunakan template. Masih ada error ataupun salah logika

--- lego_cms/src/app/admin/layout.tsx
+++ lego_cms/src/app/admin/layout.tsx
@@ -1,12 +1,8 @@
-import { Children } from "react";
-import Trends from "./trends/page";
 import NavbarAdmin from "@/component/navbar_admin";
+
 
 export default function AdminLayout(){
     return(
-        <div>
             <NavbarAdmin/>
-            <Trends/>
-        </div>
     )
 }
--- lego_cms/src/app/admin/trends/page.tsx
+++ lego_cms/src/app/admin/trends/page.tsx
@@ -1,72 +1,16 @@
 "use client"
 
-import NavbarAdmin from "@/component/navbar_admin";
-import { useState } from "react";
-import { Collapse } from "react-bootstrap";
+import SideBarAdmin from "@/component/sidebar_admin"
+
 
 export default function Trends() {
-    const [showTrends, setShowTrends] = useState(false);
     return (
-        <div>
-            { }
-            <div className="row">
-                <div className="col-2">
-                    <ul>
-                        <li className="nav-item">
-                            <a
-                                onClick={() => setShowTrends(!showTrends)}
-                                aria-controls="trends-collapse"
-                                aria-expanded={showTrends}
-                            >
-                                Trends
-                            </a>
-                            <Collapse in={showTrends}>
-                                {
-                                    <div id="trends-collapse">
-                                        <ul>
-                                            <li>
-                                                Product
-                                            </li>
-                                            <li>
-                                                Stocks
-                                            </li>
-                                            <li>
-                                                User
-                                            </li>
-                                            <li>
-                                                Review
-                                            </li>
-                                        </ul>
-                                    </div>
-                                }
-                            </Collapse>
-                        </li>
-                        <li className="nav-item">
-                            <a>
-                                Mails
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a>
-                                Users
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a>
-                                Article
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a>
-                                Stocks
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <div className="col-7">
-                    Ini Content
-                </div>
-            </div>
-        </div>
+        <SideBarAdmin>
+            {
+                <p>
+                    a
+                </p>
+            }
+        </SideBarAdmin>
     )
 }

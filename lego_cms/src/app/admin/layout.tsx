6144dd631129b90326d6a146af7d9b8823f595be
ARYASUTA22 <aryasuta.535240138@stu.untar.ac.id>
2025-11-18 10:25:27 +0700

    Commit 3: Menambahkan fitur sidebar pada dashboard.

--- lego_cms/src/app/admin/layout.tsx
+++ lego_cms/src/app/admin/layout.tsx
@@ -1,9 +1,11 @@
 import { Children } from "react";
 import Trends from "./trends/page";
+import NavbarAdmin from "@/component/navbar_admin";
 
 export default function AdminLayout(){
     return(
         <div>
+            <NavbarAdmin/>
             <Trends/>
         </div>
     )
--- lego_cms/src/app/admin/trends/page.tsx
+++ lego_cms/src/app/admin/trends/page.tsx
@@ -1,16 +1,70 @@
-import NavbarAdmin from "@/component/navbar_admin";
+"use client"
 
-export default function Trends(){
-    return(
+import NavbarAdmin from "@/component/navbar_admin";
+import { useState } from "react";
+import { Collapse } from "react-bootstrap";
+
+export default function Trends() {
+    const [showTrends, setShowTrends] = useState(false);
+    return (
         <div>
-            <NavbarAdmin/>
-            {/* Sidebar */}
+            { }
             <div className="row">
-                <div className="col-3">
-                    
+                <div className="col-2">
+                    <ul>
+                        <li className="nav-item">
+                            <a
+                                onClick={() => setShowTrends(!showTrends)}
+                                aria-controls="trends-collapse"
+                                aria-expanded={showTrends}
+                            >
+                                Trends
+                            </a>
+                            <Collapse in={showTrends}>
+                                {
+                                    <div id="trends-collapse">
+                                        <ul>
+                                            <li>
+                                                Product
+                                            </li>
+                                            <li>
+                                                Stocks
+                                            </li>
+                                            <li>
+                                                User
+                                            </li>
+                                            <li>
+                                                Review
+                                            </li>
+                                        </ul>
+                                    </div>
+                                }
+                            </Collapse>
+                        </li>
+                        <li className="nav-item">
+                            <a>
+                                Mails
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a>
+                                Users
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a>
+                                Article
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a>
+                                Stocks
+                            </a>
+                        </li>
+                    </ul>
                 </div>
-                <div className="col-5">
-                Ini Content
+                <div className="col-7">
+                    Ini Content
                 </div>
             </div>
         </div>
